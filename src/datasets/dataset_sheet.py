21469baf004397e51b9c884f86b610c6760af230
init commit <vst515@yandex.ru>
2021-10-26 13:45:44 +0300

    Fix paths in dataset.csv

--- src/datasets/dataset_sheet.py
+++ src/datasets/dataset_sheet.py
@@ -9,4 +9,4 @@ with open('dataset.csv', 'a') as file:
     for idx, path_to_file in enumerate(glob('Emotions/*/*.wav')):
         class_name = path_to_file.split('/')[IDX_CLASS_NAME_IN_PATH]
         class_number = CLASS_NAME.index(class_name)
-        file.write(f'{idx}{DELIMITER}{path_to_file}{DELIMITER}{class_name}{DELIMITER}{class_number}\n')
+        file.write(f'{idx}{DELIMITER}./datasets/{path_to_file}{DELIMITER}{class_name}{DELIMITER}{class_number}\n')
